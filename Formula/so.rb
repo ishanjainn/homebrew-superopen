c175bd9ce29d603c09fb17aee519c9f4c952691b
ishanjainn <ishan.jain0099@gmail.com>
2026-08-06 20:36:51 -0400

    so 0.1.0: add real checksums for first release

--- Formula/so.rb
+++ Formula/so.rb
@@ -15,22 +15,22 @@ class So < Formula
   on_macos do
     on_arm do
       url "https://github.com/ishanjainn/superopen/releases/download/cli-#{version}/so-darwin-arm64.tar.gz"
-      sha256 "PENDING_FIRST_RELEASE"
+      sha256 "e3789e42a5015df2e81be244d8d6d2d4c597a0799f5823ac891b84c0fa37e684"
     end
     on_intel do
       url "https://github.com/ishanjainn/superopen/releases/download/cli-#{version}/so-darwin-amd64.tar.gz"
-      sha256 "PENDING_FIRST_RELEASE"
+      sha256 "d4a0fb5c740097498e52176b0bd926426d01a6b8b226bc74345d46fed74cd517"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ishanjainn/superopen/releases/download/cli-#{version}/so-linux-arm64.tar.gz"
-      sha256 "PENDING_FIRST_RELEASE"
+      sha256 "90bb771bf66c95fb64a0f011e37c0494aea191bc277bb833f55ec9017e7955a1"
     end
     on_intel do
       url "https://github.com/ishanjainn/superopen/releases/download/cli-#{version}/so-linux-amd64.tar.gz"
-      sha256 "PENDING_FIRST_RELEASE"
+      sha256 "064a520b93781bce632ad96c8e02b55cdda1cd278dd7a8f155cd0780409c95ee"
     end
   end
 
